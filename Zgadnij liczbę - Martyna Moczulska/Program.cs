dd4ddfc0ec1fc2aed85cd92f0ce0b8e4c97f177b
Enter <Enter@DESKTOP-UQNSLE6>
2022-03-10 23:15:26 +0100

    Dodaj pliki projektów.

---
+++ Zgadnij liczbę - Martyna Moczulska/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random r = new Random();
+int liczba = r.Next(1, 10);
+int liczba2;
+Console.WriteLine("Konsola wylosowała liczbę od 1-10");
+Console.WriteLine("Zgadnij jaka to liczba");
+liczba2 = Convert.ToInt32(Console.ReadLine());
+while (liczba2 != liczba) 
+{
+    if (liczba2 > liczba)
+    {
+        Console.WriteLine("Niestety liczba którą próbowałeś zgadnąć jest mniejsza niż wylosowana!");
+        Console.WriteLine("Spróbuj ponownie!");
+        liczba2 = Convert.ToInt32(Console.ReadLine());
+    }
+    else
+    if (liczba2 < liczba)
+    {
+        Console.WriteLine("Niestety liczba którą próbowałeś zgadnąć jest większa niż wylosowana!");
+        Console.WriteLine("Spróbuj ponownie!");
+        liczba2 = Convert.ToInt32(Console.ReadLine());
+    }
+}
+if (liczba2 == liczba)
+{
+    Console.WriteLine("Brawo udało ci się!!");
+    Console.WriteLine("Wylosowana liczba to " + liczba);
+}
